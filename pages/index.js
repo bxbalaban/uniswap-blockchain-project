dc739c035055c86e98c27dc9b59d086cb1456e76
bxbalaban <balabanbsr@gmail.com>
2022-06-08 15:36:40 +0300

    TransactionHistory design implemented

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import TransactionHistory from '../components/TransactionHistory'
 
 const style={
   wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between',
@@ -12,7 +13,7 @@ const Home = () => {
     <div className={style.wrapper}>
       <Header/>
       <Main></Main>
-      <h2>Transaction History</h2>
+      <TransactionHistory/>
     </div>
   )
 }
